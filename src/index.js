d1da78fedfac34a1d8145d3de29571cd280cffe1
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-16 09:43:46 +0100

    correction in functions and index file

--- src/index.js
+++ src/index.js
@@ -6,52 +6,53 @@ const questionModule = require("./questionModule");
 async function main() {
   try {
     //Employes
-    const addEmploye = await employeModule.addEmploye(
+    await employeModule.addEmploye(
       7,
       "Aichetou Gaye",
       "Prise en contact"
     );
-    const addEmploye2 = await employeModule.addEmploye(
+    await employeModule.addEmploye(
       8,
       "Thillo Seme",
       "Inscription de donnée"
     );
-    const addEmploye4 = await employeModule.addEmploye(
+    await employeModule.addEmploye(
       6,
       "Fatima Cissé",
       "Prise de decision"
     );
     console.log("Added successfully");
 
-    const upEmploye = await employeModule.updateEmploye(
+    await employeModule.updateEmploye(
       6,
       "Fatima Sall",
       "Mangement projet"
     );
     console.log("Updated successfully");
 
-    const deleteEmploye = await employeModule.deleteEmploye(1);
+    await employeModule.deleteEmploye(1);
     console.log(`Deleted Successfully`);
+
     const listEmployes = await employeModule.listEmployes();
     console.log(listEmployes);
 
     //Surveys
 
-    const addSurvey = await surveyModule.addSurvey(
+    await surveyModule.addSurvey(
       1,
       "Enquete 001",
       "Enquete de mentalité",
       "2024-11-09",
       2
     );
-    const addSurvey2 = await surveyModule.addSurvey(
+    await surveyModule.addSurvey(
       2,
       "Enquete 002",
       "Enquete de moralité",
       "2024-10-09",
       3
     );
-    const addSurvey4 = await surveyModule.addSurvey(
+    await surveyModule.addSurvey(
       4,
       "Enquete 004",
       "Enquete de pull-up",
@@ -60,7 +61,7 @@ async function main() {
     );
     console.log("Added successfully");
 
-    const upSurvey = await surveyModule.updateSurvey(
+    await surveyModule.updateSurvey(
       3,
       "Enquête 003",
       "Enquête de satisfaction",
@@ -69,26 +70,27 @@ async function main() {
     );
     console.log("Updated successfully");
 
-    const deleteSurvey = await surveyModule.deleteSurvey(14);
+    await surveyModule.deleteSurvey(14);
     console.log(`Deleted Successfully`);
-    const listSurvey = await surveyModule.listSurveys();
-    console.log(listSurvey);
+
+    const listSurveys = await surveyModule.listSurveys();
+    console.log(listSurveys);
 
     //Questions
 
-    const addQuestion = await questionModule.addQuestion(
+    await questionModule.addQuestion(
       6,
       "Seriez-vous pret a apporter votre aide ?",
       "boolean",
       2
     );
-    const addQuestion2 = await questionModule.addQuestion(
+    await questionModule.addQuestion(
       8,
       "Que voulez vous comme amélioration ?",
       "multiplechoice",
       4
     );
-    const addQuestion4 = await questionModule.addQuestion(
+    await questionModule.addQuestion(
       9,
       "Qu'est ce manque le plus selon vous?",
       "singlechoice",
@@ -96,7 +98,7 @@ async function main() {
     );
     console.log("Added successfully");
 
-    const upQuestion = await questionModule.updateQuestion(
+    await questionModule.updateQuestion(
       9,
       "Quelle est le principal secteur actif",
       "singlechoice",
@@ -104,15 +106,16 @@ async function main() {
     );
     console.log("Updated successfully");
 
-    const deleteQuestion = await questionModule.deleteQuestion(9);
+    await questionModule.deleteQuestion(9);
     console.log(`Deleted Successfully`);
-    const listQuestion = await questionModule.listQuestions();
-    console.log(listQuestion);
+
+    const listQuestions = await questionModule.listQuestions();
+    console.log(listQuestions);
 
     //Answers
 
-    const addAnswer = await answerModule.addAnswer(6, ["Oui", "Non"], 7);
-    const addAnswer2 = await answerModule.addAnswer(
+    await answerModule.addAnswer(6, ["Oui", "Non"], 7);
+    await answerModule.addAnswer(
       7,
       [
         "Plus d'options de produits",
@@ -122,7 +125,7 @@ async function main() {
       ],
       8
     );
-    const addAnswer4 = await answerModule.addAnswer(
+    await answerModule.addAnswer(
       9,
       [
         "Qualité du service",
@@ -134,7 +137,7 @@ async function main() {
     );
     console.log("Added successfully");
 
-    const upAnswer = await answerModule.updateAnswer(
+    await answerModule.updateAnswer(
       9,
       [
         "Qualité du service",
@@ -146,10 +149,11 @@ async function main() {
     );
     console.log("Updated successfully");
 
-    const deleteAnswer = await answerModule.deleteAnswer(4);
+    await answerModule.deleteAnswer(4);
     console.log(`Deleted Successfully`);
-    const listAnswer = await answerModule.listAnswers();
-    console.log(listAnswer);
+
+    const listAnswers = await answerModule.listAnswers();
+    console.log(listAnswers);
 
   } catch (error) {
     console.log(error.message);
